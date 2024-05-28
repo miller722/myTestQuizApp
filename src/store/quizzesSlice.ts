2f58cfa1c493b022000245f098c8c41671b57860
miller <18mishak2000@gmail.com>
2024-05-28 19:40:02 +0300

    update quizzesSlice

--- src/store/quizzesSlice.ts
+++ src/store/quizzesSlice.ts
@@ -38,7 +38,7 @@ const quizzesSlice = createSlice({
                     id: uuidv4(),
                     quizId: action.payload.quizId,
                     answers: [],
-                    score: 0,
+                    score: 1,
                     type: 'single', // default type
                 } as Question;
                 quiz.questions.push(newQuestion);
@@ -51,12 +51,6 @@ const quizzesSlice = createSlice({
                 if (questionIndex !== -1) {
                     quiz.questions[questionIndex] = action.payload.question;
                 }
-            }
-        },
-        deleteQuestion: (state, action: PayloadAction<{ quizId: string; questionId: string }>) => {
-            const quiz = state.quizzes.find(quiz => quiz.id === action.payload.quizId);
-            if (quiz) {
-                quiz.questions = quiz.questions.filter(q => q.id !== action.payload.questionId);
             }
         },
         addAnswer: (state, action: PayloadAction<{ quizId: string; questionId: string; answer: Partial<Answer> }>) => {
@@ -73,28 +67,8 @@ const quizzesSlice = createSlice({
                     question.answers.push(newAnswer);
                 }
             }
-        },
-        editAnswer: (state, action: PayloadAction<{ quizId: string; questionId: string; answer: Answer }>) => {
-            const quiz = state.quizzes.find(quiz => quiz.id === action.payload.quizId);
-            if (quiz) {
-                const question = quiz.questions.find(q => q.id === action.payload.questionId);
-                if (question) {
-                    const answerIndex = question.answers.findIndex(a => a.id === action.payload.answer.id);
-                    if (answerIndex !== -1) {
-                        question.answers[answerIndex] = action.payload.answer;
-                    }
-                }
-            }
-        },
-        deleteAnswer: (state, action: PayloadAction<{ quizId: string; questionId: string; answerId: string }>) => {
-            const quiz = state.quizzes.find(quiz => quiz.id === action.payload.quizId);
-            if (quiz) {
-                const question = quiz.questions.find(q => q.id === action.payload.questionId);
-                if (question) {
-                    question.answers = question.answers.filter(a => a.id !== action.payload.answerId);
-                }
-            }
-        },
+        }
+
     },
 });
 
@@ -104,10 +78,7 @@ export const {
     deleteQuiz,
     addQuestion,
     editQuestion,
-    deleteQuestion,
     addAnswer,
-    editAnswer,
-    deleteAnswer,
 } = quizzesSlice.actions;
 
 export default quizzesSlice.reducer;
